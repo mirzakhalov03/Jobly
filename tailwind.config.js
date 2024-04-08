cfb5c1465620658a1352af7ab305c05f235e5045
Mirzakhalov Javohir <mn.afridi@Mirzakhalov-Javohirs-MacBook-Pro.local>
2024-04-08 07:36:31 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      'inter' : ['Inter', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
